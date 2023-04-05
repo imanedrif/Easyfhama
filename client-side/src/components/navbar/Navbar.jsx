edb1b99ca204c1fa84cd403d5c5b2b7f778ba313
Imane Drif <imanedrif@Imanes-MBP.mshome.net>
2023-04-05 13:06:47 +0000

    update

---
+++ client-side/src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <nav>
+            <li>
+                <ul>Cours</ul>
+                <ul>Cours</ul>
+                <ul>Cours</ul>
+            </li>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
